d08eb1f12a6f677aaa2753b9651b8bf2e5b81df4
Aleksandr <abyzovaleksandr5@gmail.com>
2023-01-03 22:26:45 +0500

    feat: task implement function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let a = Math.abs(n).toString();
+    let b = a.split('').reverse().join('');
+    return b;
 }
